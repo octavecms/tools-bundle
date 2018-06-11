3ea00b0cad0185a58122870ccd83fa88b9203337
Igor Lukashov <igor.lukashov@videinfra.com>
2018-06-11 18:06:00 +0300

    Config bugfix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('vide_infra_tools');
+        $rootNode = $treeBuilder->root('octave_tools');
 
         $rootNode
             ->children()
