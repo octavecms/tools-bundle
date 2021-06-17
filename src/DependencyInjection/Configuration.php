5e654128c23c5c8c28c8856ad05ba7595bf0b6c3
Igor Lukashov <igor.lukashov@videinfra.com>
2021-06-17 19:10:46 +0300

    configuration minor fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('octave_tools');
+        $treeBuilder = new TreeBuilder('octave_tools');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
