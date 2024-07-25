63a4d2383f2db85acac762ced67ab8ad9af25934
Daler Boboev <daler.boboev@videinfra.com>
2024-07-25 10:03:39 +0300

    redis helper service inject fix

--- src/OctaveToolsBundle.php
+++ src/OctaveToolsBundle.php
@@ -2,6 +2,8 @@
 
 namespace Octave\ToolsBundle;
 
+use Octave\ToolsBundle\DependencyInjection\Compiler\CacheAwareAdminCompiler;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -9,5 +11,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class OctaveToolsBundle extends Bundle
 {
-
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new CacheAwareAdminCompiler());
+    }
 }
