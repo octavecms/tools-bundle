4d79a1f5a42866a8949a282df86f3c0ce8e3d563
Igor Lukashov <igor.lukashov@videinfra.com>
2024-10-29 20:59:14 +0300

    cache extensions

--- src/Model/CacheExtensionInterface.php
+++ src/Model/CacheExtensionInterface.php
@@ -2,6 +2,8 @@
 
 namespace Octave\ToolsBundle\Model;
 
+use Symfony\Component\HttpFoundation\Request;
+
 interface CacheExtensionInterface
 {
     public function apply(string $key, array $options, Request $request): void;
