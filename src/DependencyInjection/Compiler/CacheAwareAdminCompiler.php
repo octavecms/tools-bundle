cb8c7dd603e3ddab5533424bacaa7ac1dd77e2a9
Igor Lukashov <igor.lukashov@videinfra.com>
2025-01-06 14:43:00 +0300

    redis cache minor fix

--- src/DependencyInjection/Compiler/CacheAwareAdminCompiler.php
+++ src/DependencyInjection/Compiler/CacheAwareAdminCompiler.php
@@ -12,7 +12,7 @@ class CacheAwareAdminCompiler implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if (!class_exists(TaggedAdminInterface::class)) {
+        if (!interface_exists(TaggedAdminInterface::class)) {
             return;
         }
 
